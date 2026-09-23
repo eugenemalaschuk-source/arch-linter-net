86efa8192dc0eb5f12874629ee7bc69c307cc6b2
Eugene Malashchuk <emalashchuk@kyriba.com>
2026-09-23 15:48:19 +0200

    chore: pin badge promotion to refreshed producer

--- tools/release/tests/test_architecture_health_badge_publisher.py
+++ tools/release/tests/test_architecture_health_badge_publisher.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parents[3]
 WORKFLOWS = ROOT / ".github" / "workflows"
 STAGE_B_ACTION_REF = (
     "eugenemalaschuk-source/arch-linter-net/.github/actions/architecture-health-badge-promotion"
-    "@289c28da05db91aad11926ee4a392204e308ca1f"
+    "@100627ff8e06e4f563a9b0e6aee19f00deae890c"
 )
 
 
